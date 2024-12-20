84870957532943f01a7172d4b3c2eea24c1bac16
OleksandrPokhodenko <160655734+OleksandrPokhodenko@users.noreply.github.com>
2024-12-20 19:46:40 +0200

    Add files via upload

---
+++ lesson_20/js/script.js
@@ -0,0 +1,13 @@
+
+let documentActions = (e) => {
+	const targetElement = e.target
+
+	if (targetElement.closest('.menu-burger')) {
+		document.documentElement.classList.toggle('open-menu')
+	}
+	else if (targetElement.closest('.open-menu')) {
+		document.documentElement.classList.remove('open-menu')
+	}
+}
+
+document.addEventListener('click', documentActions)
